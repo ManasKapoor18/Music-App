7234cba8030c561767e9af4a1efc06cf8e127c96
ManasKapoor18 <manasmk2016@gmail.com>
2024-08-01 14:17:38 +0530

    added header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/sidebar";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="min-h-screen bg-gray-900 text-white flex">
         <Sidebar />
-        {children}
+        <div className="bg-gray-800 mx-12 justify-center items-center">
+          <Header />
+          <main className="px-6 py-2 flex-1 overflow-y-auto">{children}</main>
+        </div>
       </body>
     </html>
   );
---
+++ src/components/header.tsx
@@ -0,0 +1,38 @@
+import {
+  FaBars,
+  FaChevronLeft,
+  FaChevronRight,
+  FaEllipsisH,
+  FaSearch,
+} from "react-icons/fa";
+
+const Header = () => {
+  return (
+    <header className="p-4">
+      <div className="flex flex-row">
+        <button className="ml-6 mr-2 ">
+          <FaChevronLeft size={18} />
+        </button>
+        <button className="mr-6 ml-1">
+          <FaChevronRight size={18} />
+        </button>
+        <button className="md:hidden">
+          <FaBars />
+        </button>
+        <div className="flex items-center h-[40px] rounded-full bg-white text-gray-700 px-4 w-full md:w-[80%]">
+          <FaSearch className="mr-2" />
+          <input
+            type="text"
+            placeholder="Search for artists, songs, or albums"
+            className="outline-none text-sm text-gray-700 w-full md:w-64"
+          />
+        </div>
+        <button className="ml-4">
+          <FaEllipsisH size={24} />
+        </button>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
